ced1521e99240f4c20dac9cd309548c31048b271
Ivo-Kafemann <ivokafemann@gmail.com>
2020-04-11 13:40:47 +0200

    Define function: load_data

--- sonnenmaschine.py
+++ sonnenmaschine.py
@@ -5,3 +5,17 @@ import numpy as np
 A_pv = 20
 n_pv = 0.2
 gamma = 1
+
+def load_data():
+    """
+    Loads weather data into a data frame.
+
+    Returns:
+        df - Pandas DataFrame containing global horizontal irradiance (ghi) for evergy hour of the year.
+    """
+
+    # load data file into a dataframe
+    df = pd.read_csv('input_potsdam.csv')
+
+    return df
+    print(df.head())
